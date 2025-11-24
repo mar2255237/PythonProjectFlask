ea7353d03f25af1bfc269224c165db41c2b1b00c
Mariah Maloof <mar2255237@maricopa.edu>
2025-11-23 17:04:46 -0700

    Add username and password validation for login form

--- app.py
+++ app.py
@@ -13,3 +13,18 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
+        # Input Validation Rules for Username and Password
+        if not username.isalnum():
+            output = "Username must only contain letters and numbers."
+        elif len(password) < 8:
+            output = "Password must be at least 8 characters."
+        elif not any(c.isdigit() for c in password):
+            output = "Password must have at least 1 number."
+        elif not any(not c.isalnum() for c in password):
+            output = "Password must contain at least 1 special character."
+        else:
+            # If both inputs are valid, go to the result page.
+            return redirect(url_for('result', username=username, password=password))
+
+
+
